7f864ed53653a7f371687f5a2097d59050754069
sergoxagile <soa.d2003@gmail.com>
2013-08-02 10:46:51 +0300

    Update 1111.js

--- 1111.js
+++ 1111.js
@@ -1,3 +1,30 @@
 (function(){
-alert('hi');
+var _experience
+, _videoPlayer
+, currentVideo;
+ 
+player = brightcove.api.getExperience();
+ 
+_experience = player.getModule(brightcove.api.modules.APIModules.EXPERIENCE);
+_videoPlayer = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+ 
+if(_experience.getReady())
+{
+initialize();
+}
+else
+{
+_experience.addEventListener(brightcove.api.events.ExperienceEvent.TEMPLATE_READY, initialize);
+}
+ 
+_experience.getExperienceID(function(pExperienceID){
+console.log(pExperienceID);
+});
+ 
+function initialize()
+{
+_videoPlayer.getCurrentVideo(function(pVideoDTO){
+console.log(pVideoDTO.displayName);
+});
+}
 }());
