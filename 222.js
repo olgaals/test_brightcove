db2330248fbc1351cb9145b8ab8bfe17b3309f16
sergoxagile <soa.d2003@gmail.com>
2013-08-06 19:56:50 +0300

    Update 222.js

--- 222.js
+++ 222.js
@@ -1,12 +1,22 @@
 (function() {
     console.log("****rertgrtrtgt*** EventPlugin.js has loaded ******************");
     times=0;
+    deleted=0;
 
  onPlayerReady=function(){
     console.log("ready");
-    
-    alert($("div[id$='bc12']").remove());
-     $('.vjs-controls').remove();  
+    if($("div[id$='bc12']").length>0){
+        $("div[id$='bc12']").remove();
+        deleted++;
+    }
+    if($("div[id$='bc12']").length>0){    
+        $('.vjs-controls').remove();  
+        deleted++;
+    }
+    times++;
+    if(times>500 || deleted>1){
+        clearInterval(interval);
+    }
      
     
 }
@@ -14,7 +24,7 @@
 player = brightcove.api.getExperience();
     videoPlayer = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
     experience = player.getModule(brightcove.api.modules.APIModules.EXPERIENCE);
-    //interval=setInterval(onPlayerReady, 10);
-  onPlayerReady;
+    interval=setInterval(onPlayerReady, 10);
+
         
 }());
