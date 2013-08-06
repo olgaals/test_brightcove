4bade82ece498a0d8706f296498f56fa365b4ffd
sergoxagile <soa.d2003@gmail.com>
2013-08-06 13:30:21 +0300

    Update 1111.js

--- 1111.js
+++ 1111.js
@@ -10,8 +10,7 @@
         videoPlayer.addEventListener(brightcove.api.events.MediaEvent.MEDIA_ERROR, mediaEventHandler);
         videoPlayer.addEventListener(brightcove.api.events.MediaEvent.COMPLETE, mediaEventHandler);
         videoPlayer.addEventListener(brightcove.api.events.CuePointEvent.CUE, cuePointHandler);
-        id='$bc13';
-        $('#'+id)[0].remove();
+        $("#$bc13")[0].remove();
     }
 
     // Show video and rendition info here since BEGIN fires once and only once per video.
