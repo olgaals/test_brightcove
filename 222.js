acd634df94f86d9e5193ed3579a6fef7999d85ad
sergoxagile <soa.d2003@gmail.com>
2013-08-06 17:53:22 +0300

    Update 222.js

--- 222.js
+++ 222.js
@@ -4,13 +4,10 @@
 
 function onPlayerReady(){
     console.log("ready");
+     $("div[id$='bc12']").remove();
+         $('.vjs-controls').remove();
         
 }
-
-$('document').ready(function(){
-    $("div[id$='bc12']").remove();
-         $('.vjs-controls').remove();
-});
 
 player = brightcove.api.getExperience();
     videoPlayer = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
