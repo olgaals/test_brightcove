a0afe1087bf3232e391d8b7a3ffb2fb339acb071
sergoxagile <soa.d2003@gmail.com>
2013-08-06 17:10:36 +0300

    Update 222.js

--- 222.js
+++ 222.js
@@ -4,7 +4,7 @@
 
 function onPlayerReady(){
     console.log("ready");
-         $("div[id$='bc11']")[0].remove();
+         $("div[id$='bc11']").remove();
          $('.vjs-controls').remove(); 
 }
 $('document').load(function(){
@@ -13,5 +13,9 @@ $('document').load(function(){
 player = brightcove.api.getExperience();
     videoPlayer = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
     experience = player.getModule(brightcove.api.modules.APIModules.EXPERIENCE);
-    experience.addEventListener(brightcove.player.events.ExperienceEvent.TEMPLATE_READY, onPlayerReady);
+    if (experience.getReady()) {
+        onPlayerReady();
+    } else {
+        experience.addEventListener(brightcove.player.events.ExperienceEvent.TEMPLATE_READY, onPlayerReady);
+    }
 }());
